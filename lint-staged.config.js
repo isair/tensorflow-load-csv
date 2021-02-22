f26d114a1209fc8db34e469c9fe24e9591f081ab
Baris Sencan <baris.sncn@gmail.com>
2021-02-22 10:43:16 +0000

    docs(gh-pages): Update docs

--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.{js,ts,json}': ['eslint --fix', 'jest --findRelatedTests'],
-  '*.md': 'eslint --fix',
-};
--- tests/setup.ts
+++
@@ -1,5 +0,0 @@
-import { toBeDeepCloseTo, toMatchCloseTo } from 'jest-matcher-deep-close-to';
-
-console.warn = jest.fn();
-
-expect.extend({ toBeDeepCloseTo, toMatchCloseTo });
